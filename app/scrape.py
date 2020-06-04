77abb7344c2d2ccb3e3d970618f4e1743c252875
ChetanB47B <chetan.borse@47billion.com>
2020-06-04 21:44:02 +0530

    solve the scrapping issues

--- app/scrape.py
+++ app/scrape.py
@@ -6,9 +6,22 @@ import os
 from flask import flash
 
 def get_product_info(asin):
+    # headers = {
+    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36'}
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36'}
-    XPATH_TOTAL_REVIEWS = './/span[@data-hook="total-review-count"]//text()'
+    'authority': 'www.amazon.com',
+    'pragma': 'no-cache',
+    'cache-control': 'no-cache',
+    'dnt': '1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'none',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-dest': 'document',
+    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+}    
+    XPATH_TOTAL_REVIEWS = './/div[@data-hook="total-review-count"]//text()'
     XPATH_PRODUCT_NAME = './/a[@data-hook="product-link"]//text()'
     XPATH_PRODUCT_RATING = './/span[@data-hook="rating-out-of-text"]//text()'
     amazon_url = 'https://www.amazon.com/product-reviews/' + asin + '/ref=cm_cr_arp_d_paging_btm_1?pageNumber=1&sortBy=recent'
@@ -20,7 +33,7 @@ def get_product_info(asin):
     product_name = product_name[0]
     product_rating = parser.xpath(XPATH_PRODUCT_RATING)
     product_rating = product_rating[0].split()[0]
-    totalreviews = int(totalreviews[0].replace(',',''))
+    totalreviews = int(totalreviews[0].replace(',','').split(' ')[0])
     product_info = [product_name, totalreviews, product_rating]
     return product_info
 
@@ -29,29 +42,43 @@ def scrape_reviews(asin):
         ratings_dict = {}
         reviews_list = []
         reviews_df = pd.DataFrame()
+        # headers = {
+        #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36'}
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36'}
+        'authority': 'www.amazon.com',
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'dnt': '1',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.62 Safari/537.36',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'none',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-dest': 'document',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        }
         XPATH_REVIEWS = '//div[@data-hook="review"]'
         XPATH_REVIEW_RATING = './/i[@data-hook="review-star-rating"]//text()'
-        XPATH_REVIEW_HEADER = './/a[@data-hook="review-title"]//text()'
-        XPATH_REVIEW_AUTHOR = './/a[@data-hook="review-author"]//text()'
+        XPATH_REVIEW_HEADER = './/a[@data-hook="review-title"]//span//text()'
+        XPATH_REVIEW_AUTHOR = './/div[@class= "a-profile-content"]//span//text()'
         XPATH_REVIEW_DATE = './/span[@data-hook="review-date"]//text()'
-        XPATH_REVIEW_BODY = './/span[@data-hook="review-body"]//text()'
+        XPATH_REVIEW_BODY = './/span[@data-hook="review-body"]//span//text()'
         XPATH_REVIEW_HELPFUL = './/span[@data-hook="helpful-vote-statement"]//text()'
-        XPATH_REVIEW_PAGENUM = './/li[@class="page-button"]//text()'
-        XPATH_TOTAL_REVIEWS = './/span[@data-hook="total-review-count"]//text()'
-        amazon_page1 = 'https://www.amazon.com/product-reviews/' + asin + '/ref=cm_cr_arp_d_paging_btm_1?pageNumber=1&sortBy=recent'
+        XPATH_REVIEW_PAGENUM = '//span[@data-hook="cr-filter-info-review-count"]//text()'
+        XPATH_TOTAL_REVIEWS = './/div[@data-hook="total-review-count"]//text()'
+        amazon_page1 = 'https://www.amazon.com/dp/product-reviews/' + asin + '/ref=cm_cr_arp_d_paging_btm_prev_1?pageNumber=1&sortBy=recent'
         p_num = 1
         page = requests.get(amazon_page1, headers=headers)
         page_response = page.text.encode('utf-8')
         parser = html.fromstring(page_response)
         pages = parser.xpath(XPATH_REVIEW_PAGENUM)
         totalreviews = parser.xpath(XPATH_TOTAL_REVIEWS)
-        totalreviews = int(totalreviews[0].replace(",",""))
-        maxpage = int(pages[-1].replace(",",""))
-        totalpages = maxpage
+        totalreviews = int(totalreviews[-1].replace(",","").split(' ')[0] )
+        maxpage = int(pages[-1].replace(",","").split(' ')[-2] )
+        totalpages = maxpage//10
+        # totalpages = 3
         while True:
-            amazon_url = 'https://www.amazon.com/product-reviews/' + asin + '/ref=cm_cr_arp_d_paging_btm_' +str(p_num) + '?pageNumber=' + str(p_num) + '&sortBy=recent'
+            amazon_url = 'https://www.amazon.com/product-reviews/' + asin + '/ref=cm_cr_arp_d_paging_btm__next_' +str(p_num) + '?pageNumber=' + str(p_num) + '&sortBy=recent'
             page = requests.get(amazon_url, headers=headers)
             page_response = page.text.encode('utf-8')
             parser = html.fromstring(page_response)
@@ -85,8 +112,10 @@ def scrape_reviews(asin):
                                         .str.replace(r'[^0-9]', ''))
         reviews_df['review_helpful'].loc[reviews_df['review_helpful'] == ''] = '0'
         reviews_df['review_helpful'] = reviews_df['review_helpful'].astype(int)
+        reviews_df['review_posted_date'] =reviews_df['review_posted_date'].apply(lambda x: x.split("on", 1)[1])
         reviews_df['review_posted_date'] = pd.to_datetime(reviews_df['review_posted_date']
                                                           .str.strip('on'))
+        
         reviews_df['review_rating'] = reviews_df['review_rating'].str.strip('out of 5 stars').astype(float)
         reviews_df.loc[reviews_df['review_rating'] == 0, 'review_rating'] = 5
         reviews_df['review_length'] = reviews_df['review_text'].apply(lambda x: len(x))
